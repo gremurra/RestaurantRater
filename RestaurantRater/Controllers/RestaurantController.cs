00077acc9574e62d5cc7456eed5da0db172e1575
Greg Murray <gremurra@gmail.com>
2020-05-11 14:26:53 -0400

    Create functionality added

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -11,10 +11,32 @@ namespace RestaurantRater.Controllers
     {
 
         private RestaurantDbContext _db = new RestaurantDbContext();
+
         // GET: Restaurant / Index
         public ActionResult Index()
         {
             return View(_db.Restaurants.ToList());
+        }
+
+        // GET: Restaurant / Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurant / Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
         }
     }
 }
